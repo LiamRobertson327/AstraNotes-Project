31c86f3e28695db29acc66622c40142b32936d6d
LiamRobertson327 <119238299+LiamRobertson327@users.noreply.github.com>
2026-05-25 22:22:49 -0700

    Removing MSVC-specific compatibility header.

--- src/compat/msvc_qt_compat.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Qt 6.5.x can require stdext-related declarations on newer MSVC toolchains
-// when compiling in latest standard mode. Pulling in <iterator> early restores
-// those declarations for Qt headers.
-#include <iterator>
